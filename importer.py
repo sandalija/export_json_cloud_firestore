dd4e501213783c841bb53e56bc25e8581162f9b7
Sergio Candalija Valderrama <sergiocava.d@gmail.com>
2020-09-12 18:54:38 +0200

    print keys

--- importer.py
+++ importer.py
@@ -5,6 +5,7 @@ from firebase_admin import firestore
 import datetime
 import json
 import random
+import string
 
 
 cred = credentials.Certificate('credentials.json')
@@ -16,14 +17,26 @@ paths = ['events']
 today = datetime.datetime.now()
 element_ids = []
 
+def get_random_alphanumeric_string(length):
+    letters_and_digits = string.ascii_letters + string.digits
+    return ''.join((random.choice(letters_and_digits) for i in range(length)))
+
 for path in paths:
     event_files = os.listdir('data/' + path)
     for file in event_files:
-        element_ref = db.collection(path).document
         event = open('data/' + path + '/' + file,'r')
         event_dict = json.load(event)
+
         start_offset = random.randint(0, 14)
         event_dict['start_time'] = (today + datetime.timedelta(days = start_offset)).isoformat()
         event_dict['end_time'] = (today + datetime.timedelta(days = random.randint(0, 14) + start_offset)).isoformat()
-        element_ids.append(element_ref)
+        event_dict['name'] = "[imported] " + event_dict['name']
+
+        key = get_random_alphanumeric_string(20)
+        element_ref = db.collection(path).document(key)
+        element_ref.set(event_dict)
+        element_ids.append(key)
+        
+        print ("Added " + path +" (ID: " + key + ")")
+
 
